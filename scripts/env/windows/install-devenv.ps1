f38b4e2de8bd960a1653a47f49845cba3bb4cb54
Piergiorgio Vagnozzi <piergiorgio_vagnozzi@hotmail.com>
2024-10-21 09:11:59 +0200

    Initial Commit

---
+++ scripts/env/windows/install-devenv.ps1
@@ -0,0 +1,6 @@
+./install-wsl.ps1
+./install-winget-app.ps1 Docker.DockerDesktop
+./install-winget-app.ps1 Microsoft.WindowsTerminal
+./install-winget-app.ps1 Git.Git
+./install-winget-app.ps1 GitExtensionsTeam.GitExtensions
+./install-winget-app.ps1 Microsoft.VisualStudioCode
---
+++ scripts/env/windows/install-winget-app.ps1
@@ -0,0 +1,92 @@
+﻿<#
+.SYNOPSIS
+	Installs an app via winget
+
+.DESCRIPTION
+	This PowerShell script installs an app from winget.
+
+.PARAMETER ApplicationId
+    The ID of the application to install.
+
+.PARAMETER ApplicationSource
+    The source from which to install the application. Default is "winget".
+
+.PARAMETER Upgrade
+    Switch to upgrade the application if it is already installed.
+
+.EXAMPLE
+	PS> ./install-winget-app.ps1 Docker.DockerDesktop --Source winget --Upgrade
+
+.LINK
+	https://github.com/pagnozzi/PowerShell
+
+.NOTES
+	Author: Piergiorgio Vagnozzi| License: CC0
+#>
+
+#Requires -RunAsAdministrator
+
+Param(
+    [Parameter(Position=0, Mandatory=$true)]
+    [string]$ApplicationId,
+    [Parameter(Mandatory=$false)]
+    [switch]$Upgrade = $false
+)
+
+function AppInstalled([string]$AppId) {    
+    $found = winget list | Where-Object { $_ -like "*$AppId*" }
+    return $found ? $true : $false
+}
+
+function AppUpgradable([string]$AppId) {    
+    $update = winget upgrade --id $AppId | Select-Object -First 1
+    return $update ? $true : $false                
+}
+
+function AppInstall([string]$AppId) {            
+    & winget.exe install --id $AppId -e --accept-source-agreements --disable-interactivity --silent --accept-package-agreements --force
+    if ($lastExitCode -ne "0") { 
+        throw "Error installing ${ApplicationId}: ${$Error[0]}"
+    }            
+}
+
+function AppUpgrade([string]$AppId) {
+    & winget.exe upgrade --id $AppId -e --accept-source-agreements --disable-interactivity --silent --accept-package-agreements
+    if ($lastExitCode -ne "0") { 
+        throw "Error installing ${ApplicationId}: ${$Error[0]}"
+    }
+}
+
+[bool]$DoUpgrade = $false
+try {
+    if (AppInstalled($ApplicationId)) {                
+        if (!$Upgrade) {
+            "✅ ${ApplicationId} already installed"
+            exit 0
+        }        
+
+        if (!AppUpgradable($ApplicationId)) {
+            "✅ ${ApplicationId} already installed and not update found"
+            exit 0
+        }
+
+        $DoUpgrade = $true
+    }    
+
+    if ($DoUpgrade) {
+        "👉 Upgrading ${ApplicationId}, please wait..."
+        AppUpgrade($ApplicationId)
+        "✅ ${ApplicationId} upgraded successfully."
+    }
+    else {     
+        "👉 Installing ${ApplicationId}, please wait..."
+        AppInstall($ApplicationId)
+        "✅ ${ApplicationId} installed successfully."
+    }
+
+}
+catch {
+    "⚠️ $_"
+    exit 1
+}
+
